c044df85416fe4b781ed55f2145f84e4e01c692b
Mehdi Rafee <mrafee113@gmail.com>
2025-07-13 21:28:42 +0330

    refactor thoroughly implement sorting

--- pkg/task/sort.go
+++ pkg/task/sort.go
@@ -3,26 +3,141 @@ package task
 import (
 	"slices"
 	"strings"
+	"time"
 )
 
-func sortNil(l, r *Task) int {
-	if l == nil && r != nil {
-		return 1
-	} else if l != nil && r == nil {
+// pay good attention to the values
+func sortNil[T any](lv, rv *T) int {
+	if lv != nil && rv == nil {
 		return -1
-	} else if l == nil && r == nil {
+	} else if lv == nil && rv != nil {
+		return 1
+	} else if lv == nil && rv == nil {
+		return 2
+	}
+	return 3
+}
+
+func sortString(lv, rv string) int {
+	if len(lv) != 0 && len(rv) == 0 {
+		return -1
+	} else if len(lv) == 0 && len(rv) != 0 {
+		return 1
+	} else if lv < rv {
+		return -1
+	} else if lv > rv {
+		return 1
+	}
+	return 2
+}
+
+func sortId(lv, rv *int) int {
+	if v := sortNil(lv, rv); v != 3 {
+		return v
+	}
+	if *lv < *rv {
+		return -1
+	} else if *lv > *rv {
+		return 1
+	}
+	return 2
+}
+
+func sortID(lv, rv *string) int {
+	if v := sortNil(lv, rv); v != 3 {
+		return v
+	}
+	if *lv < *rv {
+		return -1
+	} else if *lv > *rv {
+		return 1
+	}
+	return 2
+}
+
+func sortTime(lv, rv *time.Time) int {
+	if v := sortNil(lv, rv); v != 3 {
+		return v
+	}
+	if lva, rva := lv.After(rightNow), rv.After(rightNow); lva && !rva {
+		return -1
+	} else if !lva && rva {
+		return 1
+	}
+	if lv.Before(*rv) {
+		return -1
+	} else if lv.After(*rv) {
+		return 1
+	}
+	return 2
+}
+
+func sortReminders(lr, rr []*time.Time) int {
+	if len(lr) != 0 && len(rr) == 0 {
+		return -1
+	} else if len(lr) == 0 && len(rr) != 0 {
+		return 1
+	} else if len(lr) == 0 && len(rr) == 0 {
+		return 2
+	}
+	lr = slices.Clone(lr)
+	rr = slices.Clone(rr)
+	slices.SortFunc(lr, func(l, r *time.Time) int {
+		if v := sortTime(l, r); v != 2 {
+			return v
+		}
 		return 0
+	})
+	slices.SortFunc(rr, func(l, r *time.Time) int {
+		if v := sortTime(l, r); v != 2 {
+			return v
+		}
+		return 0
+	})
+	minLen := min(len(lr), len(rr))
+	for ndx := range minLen {
+		if v := sortTime(lr[ndx], rr[ndx]); v != 2 {
+			return v
+		}
+	}
+	if len(lr) > len(rr) {
+		return -1
+	} else if len(lr) < len(rr) {
+		return 1
+	}
+	return 2
+}
+
+func sortDatetime(l, r *Task) int {
+	if v := sortTime(l.Time.Deadline, r.Time.Deadline); v != 2 {
+		return v
+	} else if v := sortTime(l.Time.EndDate, r.Time.EndDate); v != 2 {
+		return v
+	} else if v := sortTime(l.Time.DueDate, r.Time.DueDate); v != 2 {
+		return v
+	} else if v := sortTime(l.Time.CreationDate, r.Time.CreationDate); v != 2 { // this is pointless lol
+		return v
+	} else if v := sortReminders(l.Time.Reminders, r.Time.Reminders); v != 2 {
+		return v
+	}
+	return 2
+}
+
+func sortDuration(lv, rv *time.Duration) int {
+	if v := sortNil(lv, rv); v != 3 {
+		return v
+	}
+	if *lv < *rv {
+		return -1
+	} else if *lv > *rv {
+		return 1
 	}
 	return 2
 }
 
 func sortProgressValue(l, r *Task) int {
-	if l.Prog != nil && r.Prog == nil {
-		return -1
-	} else if l.Prog == nil && r.Prog != nil {
-		return 1
-	} else if l.Prog == nil && r.Prog == nil {
-		return 2
+	if v := sortNil(l.Prog, r.Prog); v != 3 {
+		return v
 	}
 	lP, rP := 100*l.Prog.Count/l.Prog.DoneCount, 100*r.Prog.Count/r.Prog.DoneCount
 	if lP > rP {
@@ -38,66 +153,51 @@ func sortProgressValue(l, r *Task) int {
 	return 2
 }
 
-func sortCategory(l, r *Task) int {
-	if l.Prog != nil && r.Prog == nil {
-		return -1
-	} else if l.Prog == nil && r.Prog != nil {
-		return 1
-	} else if l.Prog == nil && r.Prog == nil {
-		return 2
+func sortProgressCategory(l, r *Task) int {
+	if v := sortNil(l.Prog, r.Prog); v != 3 {
+		return v
 	}
-	if l.Prog.Category != "" && r.Prog.Category == "" {
-		return -1
-	} else if l.Prog.Category == "" && r.Prog.Category != "" {
-		return 1
-	} else if l.Prog.Category < r.Prog.Category {
-		return -1
-	} else if l.Prog.Category > r.Prog.Category {
-		return 1
-	}
-	return 2
+	return sortString(l.Prog.Category, r.Prog.Category)
 }
 
 func sortPriority(l, r *Task) int {
-	if l.Priority != nil && r.Priority == nil {
-		return -1
-	} else if l.Priority == nil && r.Priority != nil {
-		return 1
-	} else if l.Priority == nil && r.Priority == nil {
-		return 2
+	if v := sortNil(l.Priority, r.Priority); v != 3 {
+		return v
 	}
-	if *l.Priority != "" && *r.Priority == "" {
-		return -1
-	} else if *l.Priority == "" && *r.Priority != "" {
-		return 1
-	} else if *l.Priority != "" && *r.Priority != "" && *l.Priority < *r.Priority {
-		return -1
-	} else if *l.Priority != "" && *r.Priority != "" && *l.Priority > *r.Priority {
-		return 1
-	}
-	return 2
+	return sortString(*l.Priority, *r.Priority)
 }
 
 func sortHints(l, r *Task) int {
-	extractPlus := func(hints []*string) []string {
+	extractPlus := func(hints []*string) (int, string, int, string) {
 		var out []string
+		var rest []string
 		for _, h := range hints {
 			if len(*h) > 0 && (*h)[0] == '+' {
 				out = append(out, *h)
+			} else {
+				rest = append(rest, *h)
 			}
 		}
 		slices.Sort(out)
-		return out
+		return len(out), strings.Join(out, ","), len(rest), strings.Join(rest, ",")
 	}
-	lHints, rHints := extractPlus(l.Hints), extractPlus(r.Hints)
-	lHint, rHint := strings.Join(lHints, ","), strings.Join(rHints, ",")
-	if len(lHints) > 0 && len(rHints) == 0 {
+	lPlusLen, lPlus, lHintsLen, lHints := extractPlus(l.Hints)
+	rPlusLen, rPlus, rHintsLen, rHints := extractPlus(r.Hints)
+	if lPlusLen > 0 && rPlusLen == 0 {
 		return -1
-	} else if len(lHints) == 0 && len(rHints) > 0 {
+	} else if lPlusLen == 0 && rPlusLen > 0 {
 		return 1
-	} else if len(lHints) > 0 && len(rHints) > 0 && lHint < rHint {
+	} else if lPlusLen > 0 && rPlusLen > 0 && lPlus < rPlus {
 		return -1
-	} else if len(lHints) > 0 && len(rHints) > 0 && lHint > rHint {
+	} else if lPlusLen > 0 && rPlusLen > 0 && lPlus > rPlus {
+		return 1
+	} else if lHintsLen > 0 && rHintsLen == 0 {
+		return -1
+	} else if lHintsLen == 0 && rHintsLen > 0 {
+		return 1
+	} else if lHintsLen > 0 && rHintsLen > 0 && lHints < rHints {
+		return -1
+	} else if lHintsLen > 0 && rHintsLen > 0 && lHints > rHints {
 		return 1
 	}
 	return 2
@@ -105,22 +205,34 @@ func sortHints(l, r *Task) int {
 
 func sortText(l, r *Task) int {
 	lText, rText := l.NormRegular(), r.NormRegular()
-	if len(lText) > 0 && len(rText) == 0 {
+	return sortString(lText, rText)
+}
+
+func sortChildren(l, r *Task) int {
+	if len(l.Children) > 0 && len(r.Children) == 0 {
 		return -1
-	} else if len(lText) == 0 && len(rText) > 0 {
+	} else if len(l.Children) == 0 && len(r.Children) > 0 {
 		return 1
-	} else if len(lText) > 0 && len(rText) > 0 && lText < rText {
-		return -1
-	} else if len(lText) > 0 && len(rText) > 0 && lText > rText {
-		return 1
+	} else if len(l.Children) > 0 && len(r.Children) > 0 {
+		lndx, rndx := 0, 0
+		for lndx < len(l.Children) && rndx < len(r.Children) {
+			if v := sortHelper(l.Children[lndx], r.Children[rndx]); v != 0 {
+				return v
+			}
+			lndx++
+			rndx++
+		}
 	}
 	return 2
 }
 
 func sortHelper(l, r *Task) int {
-	if v := sortNil(l, r); v != 2 {
+	if v := sortNil(l, r); v != 3 {
+		if v == 2 {
+			return 0
+		}
 		return v
-	} else if v = sortCategory(l, r); v != 2 {
+	} else if v = sortProgressCategory(l, r); v != 2 {
 		return v
 	} else if v = sortPriority(l, r); v != 2 {
 		return v
@@ -129,6 +241,18 @@ func sortHelper(l, r *Task) int {
 	} else if v = sortHints(l, r); v != 2 {
 		return v
 	} else if v = sortText(l, r); v != 2 {
+		return v
+	} else if v = sortDatetime(l, r); v != 2 {
+		return v
+	} else if v = sortDuration(l.Time.Every, r.Time.Every); v != 2 {
+		return v
+	} else if v = sortID(l.EID, r.EID); v != 2 {
+		return v
+	} else if v = sortID(l.PID, r.PID); v != 2 {
+		return v
+	} else if v = sortId(l.ID, r.ID); v != 2 {
+		return v
+	} else if v = sortChildren(l, r); v != 2 {
 		return v
 	}
 	return 0
--- pkg/task/sort_test.go
+++ pkg/task/sort_test.go
@@ -1,10 +1,52 @@
 package task
 
 import (
+	"math/rand"
+	"slices"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func shuffle[T any](src []T) []T {
+	dst := slices.Clone(src)
+	rand.Shuffle(len(dst), func(i, j int) {
+		dst[i], dst[j] = dst[j], dst[i]
+	})
+	return dst
+}
+
+func shuffleCount[T any](src []T, count int) [][]T {
+	var out [][]T
+	for range count {
+		out = append(out, shuffle(src))
+	}
+	return out
+}
+
+func permutations[T any](src []T) [][]T {
+	n := len(src)
+	switch n {
+	case 0:
+		return [][]T{}
+	case 1:
+		return [][]T{{src[0]}}
+	}
+	var result [][]T
+	for i := 0; i < n; i++ {
+		fixed := src[i]
+		rest := make([]T, 0, n-1)
+		rest = append(rest, src[:i]...)
+		rest = append(rest, src[i+1:]...)
+		for _, perm := range permutations(rest) {
+			newPerm := make([]T, 0, n)
+			newPerm = append(newPerm, fixed)
+			newPerm = append(newPerm, perm...)
+			result = append(result, newPerm)
+		}
+	}
+	return result
+}
 
 func TestSortTask(t *testing.T) {
 	assert := assert.New(t)
@@ -19,40 +61,33 @@ func TestSortTask(t *testing.T) {
 		}
 		arr = sortTasks(arr)
 	}
-	t.Run("doneCount", func(t *testing.T) {
-		for _, lineSet := range [][]string{{
-			"$p=unit/0/100", "random",
-		}, {
-			"random", "$p=unit/0/100",
-		}} {
-			prep(lineSet)
-			assert.Equal("$p=unit/0/100", arr[0].Norm())
-			assert.Equal("random", arr[1].Norm())
-		}
-	})
-	t.Run("category", func(t *testing.T) {
-		for _, lineSet := range [][]string{{
+	t.Run("progress", func(t *testing.T) {
+		for _, lineSet := range permutations([]string{
 			"$p=unit/0/100/cat", "$p=unit/0/100",
 			"$p=unit/0/100/catA", "$p=unit/0/100/catB",
-		}, {
-			"$p=unit/0/100", "$p=unit/0/100/cat",
-			"$p=unit/0/100/catB", "$p=unit/0/100/catA",
-		}} {
+		}) {
 			prep(lineSet)
 			assert.Equal("$p=unit/0/100/cat", arr[0].Norm())
 			assert.Equal("$p=unit/0/100", arr[3].Norm())
 			assert.Equal("$p=unit/0/100/catA", arr[1].Norm())
 			assert.Equal("$p=unit/0/100/catB", arr[2].Norm())
 		}
+		for _, lineSet := range permutations([]string{
+			"$p=unit/0/300", "$p=unit/10/100",
+			"$p=unit/20/100", "$p=unit/200/1000",
+		}) {
+			prep(lineSet)
+			assert.Equal("$p=unit/200/1000", arr[0].Norm())
+			assert.Equal("$p=unit/20/100", arr[1].Norm())
+			assert.Equal("$p=unit/10/100", arr[2].Norm())
+			assert.Equal("$p=unit/0/300", arr[3].Norm())
+		}
 	})
 	t.Run("priority", func(t *testing.T) {
-		for _, lineSet := range [][]string{{
+		for _, lineSet := range permutations([]string{
 			"(A) s", "s",
 			"(B) s", "(C) s",
-		}, {
-			"s", "(A) s",
-			"(C) s", "(B) s",
-		}} {
+		}) {
 			prep(lineSet)
 			assert.Equal("(A) s", arr[0].Norm())
 			assert.Equal("s", arr[3].Norm())
@@ -61,28 +96,41 @@ func TestSortTask(t *testing.T) {
 		}
 	})
 	t.Run("hints", func(t *testing.T) {
-		for _, lineSet := range [][]string{{
+		for _, lineSet := range shuffleCount([]string{
 			"+b +a", "s",
 			"+b +a +z", "+z +b +0",
-		}, {
-			"s", "+b +a",
-			"+z +b +0", "+b +a +z",
-		}} {
+		}, 25) {
 			prep(lineSet)
 			assert.Equal("+z +b +0", arr[0].Norm())
 			assert.Equal("+b +a", arr[1].Norm())
 			assert.Equal("+b +a +z", arr[2].Norm())
 			assert.Equal("s", arr[3].Norm())
 		}
+		for _, lineSet := range shuffleCount([]string{
+			"#a", "#ab", "#a #b #c",
+			"@a", "@ab", "@a @b @c",
+			"#a @a", "#a @b", "#b @a",
+			"@a #b", "@a #b", "@b #a",
+		}, 20) {
+			prep(lineSet)
+			assert.Equal("#a", arr[0].Norm())
+			assert.Equal("#a #b #c", arr[1].Norm())
+			assert.Equal("#a @a", arr[2].Norm())
+			assert.Equal("#a @b", arr[3].Norm())
+			assert.Equal("#ab", arr[4].Norm())
+			assert.Equal("#b @a", arr[5].Norm())
+			assert.Equal("@a", arr[6].Norm())
+			assert.Equal("@a #b", arr[7].Norm())
+			assert.Equal("@a #b", arr[8].Norm())
+			assert.Equal("@a @b @c", arr[9].Norm())
+			assert.Equal("@ab", arr[10].Norm())
+		}
 	})
 	t.Run("texts", func(t *testing.T) {
-		for _, lineSet := range [][]string{{
+		for _, lineSet := range permutations([]string{
 			"a +b", "+b",
 			"c", "d",
-		}, {
-			"+b", "a +b",
-			"d", "c",
-		}} {
+		}) {
 			prep(lineSet)
 			assert.Equal("a +b", arr[0].Norm())
 			assert.Equal("+b", arr[1].Norm())
@@ -91,25 +139,13 @@ func TestSortTask(t *testing.T) {
 		}
 	})
 	t.Run("parent children", func(t *testing.T) {
-		for _, lineSet := range [][]string{{
+		for _, lineSet := range permutations([]string{
 			"a",
 			"b $id=1",
 			"z.1 $P=1",
 			"a.2 $P=1",
 			"c",
-		}, {
-			"a.2 $P=1",
-			"a",
-			"z.1 $P=1",
-			"c",
-			"b $id=1",
-		}, {
-			"z.1 $P=1",
-			"b $id=1",
-			"c",
-			"a",
-			"a.2 $P=1",
-		}} {
+		}) {
 			path, _ := parseFilepath("test")
 			prep := func(lines []string) {
 				Lists.Empty(path)
@@ -128,6 +164,82 @@ func TestSortTask(t *testing.T) {
 			assert.Equal("a.2 $P=1", Lists[path].Tasks[2].Norm())
 			assert.Equal("z.1 $P=1", Lists[path].Tasks[3].Norm())
 			assert.Equal("c", Lists[path].Tasks[4].Norm())
+		}
+		for _, lineSet := range shuffleCount([]string{
+			"$id=0", "$id=1", "$id=2",
+			"$P=0", "$P=1", "$P=2",
+		}, 20) {
+			prep(lineSet)
+			assert.Equal("$id=0", arr[0].Norm())
+			assert.Equal("$id=1", arr[1].Norm())
+			assert.Equal("$id=2", arr[2].Norm())
+			assert.Equal("$P=0", arr[3].Norm())
+			assert.Equal("$P=1", arr[4].Norm())
+			assert.Equal("$P=2", arr[5].Norm())
+		}
+		for _, lineSet := range shuffleCount([]string{
+			"$id=0", "$id=1", "$id=2",
+			"$P=0", "$P=1", "$P=2",
+			"$id=-1", "$id=4", "$id=5",
+			"$P=-2", "$P=-3", "$P=-4",
+		}, 20) {
+			path, _ := parseFilepath("test")
+			prep := func(lines []string) {
+				Lists.Empty(path)
+				for ndx, line := range lines {
+					task, err := ParseTask(&ndx, line)
+					assert.NoError(err)
+					assert.NotNil(task)
+					Lists.Append(path, task)
+				}
+				cleanupRelations(path)
+				Lists.Sort(path)
+			}
+			prep(lineSet)
+			assert.Equal("$id=-1", Lists[path].Tasks[0].Norm())
+			assert.Equal("$id=0", Lists[path].Tasks[1].Norm())
+			assert.Equal("$P=0", Lists[path].Tasks[2].Norm())
+			assert.Equal("$id=1", Lists[path].Tasks[3].Norm())
+			assert.Equal("$P=1", Lists[path].Tasks[4].Norm())
+			assert.Equal("$id=2", Lists[path].Tasks[5].Norm())
+			assert.Equal("$P=2", Lists[path].Tasks[6].Norm())
+			assert.Equal("$id=4", Lists[path].Tasks[7].Norm())
+			assert.Equal("$id=5", Lists[path].Tasks[8].Norm())
+			assert.Equal("$P=-2", Lists[path].Tasks[9].Norm())
+			assert.Equal("$P=-3", Lists[path].Tasks[10].Norm())
+			assert.Equal("$P=-4", Lists[path].Tasks[11].Norm())
+		}
+	})
+	t.Run("datetime", func(t *testing.T) {
+		for _, lineSet := range shuffleCount([]string{
+			"$due=1w $dead=1w", "$due=1w $dead=2w",
+			"$due=1w $end=1w", "$due=1w $end=2w",
+			"$due=2d", "$due=4d",
+			"$c=2024-jan", "$c=2024-feb",
+			"$r=20d $r=1w $r=1h", "$r=1w $r=1h", "$r=10d $r=1h", "$r=1h $r=2d",
+		}, 5) {
+			prep(lineSet)
+			assert.Equal("$due=1w $dead=1w", arr[0].Norm())
+			assert.Equal("$due=1w $dead=2w", arr[1].Norm())
+			assert.Equal("$due=1w $end=1w", arr[2].Norm())
+			assert.Equal("$due=1w $end=2w", arr[3].Norm())
+			assert.Equal("$due=2d", arr[4].Norm())
+			assert.Equal("$due=4d", arr[5].Norm())
+			assert.Equal("$c=2024-jan", arr[6].Raw())
+			assert.Equal("$c=2024-feb", arr[7].Raw())
+			assert.Equal("$r=1h $r=2d", arr[8].Norm())
+			assert.Equal("$r=20d $r=1w $r=1h", arr[9].Norm())
+			assert.Equal("$r=1w $r=1h", arr[10].Norm())
+			assert.Equal("$r=10d $r=1h", arr[11].Norm())
+		}
+	})
+	t.Run("every", func(t *testing.T) {
+		for _, lineSet := range permutations([]string{
+			"$every=6m $due=3w", "$every=1y $due=3w",
+		}) {
+			prep(lineSet)
+			assert.Equal("$every=6m $due=3w", arr[0].Norm())
+			assert.Equal("$every=1y $due=3w", arr[1].Norm())
 		}
 	})
 }
